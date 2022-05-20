4d0a8beb4752b924690524c57cabc2936bd4cb60
matheusn4k4 <100139051+matheusn4k4@users.noreply.github.com>
2022-05-19 21:19:32 -0300

    Add files via upload

---
+++ routes/usuarios.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+router.get("/", function (req, res) {
+    usuarioController.testar(req, res);
+});
+
+router.get("/listar", function (req, res) {
+    usuarioController.listar(req, res);
+});
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    usuarioController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.entrar(req, res);
+});
+
+router.post("/esqueceu", function (req, res) {
+    usuarioController.esqueceu(req, res);
+});
+
+router.post("/plantacao", function (req, res) {
+    usuarioController.plantacao(req, res);
+});
+
+router.post("/novasenha", function (req, res) {
+    usuarioController.novasenha(req, res);
+});
+
+router.get("/listarPlantacoes", function (req, res) {
+    usuarioController.listarPlantacoes(req, res);
+});
+
+module.exports = router;
